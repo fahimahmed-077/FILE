1bf301750d111efe7c49697ae8400020c0a65bf1
FAHIM AHMED RAJ <fahimahmedraj207@gmail.com>
2026-05-17 22:33:46 +0600

    Update toilet.js

--- scripts/cmds/toilet.js
+++ scripts/cmds/toilet.js
@@ -67,7 +67,7 @@ module.exports = {
       );
 
     } catch (err) {
-      api.sendMessage(`🥹error, contact MahMUD.`, threadID, messageID);
+      api.sendMessage(`🥹error, contact 𝗙𝗔𝗛𝗜𝗠.`, threadID, messageID);
     }
   }
 };
